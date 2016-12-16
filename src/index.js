a62050c954844475151b4d9c4fe12a40e240ef8d
jisuyoun <nigayoun@gmail.com>
2016-12-17 06:51:34 +0900

    add. install react-router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from './Reducers/Counter.js'
 import { Provider } from 'react-redux'
+import { Router, Route, hashHistory, browserHistory, IndexRoute} from 'react-router'
 
 import App from './App';
 import './index.css';
